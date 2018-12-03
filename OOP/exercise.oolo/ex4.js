39adb434b96cf10fb8d1c9dfe6550e74cf93abec
nimaz niyas <mgha123@gmail.com>
2018-12-03 13:14:12 +0300

    exercise 4 is done with prototype inheritance

--- OOP/exercise.oolo/ex4.js
+++ OOP/exercise.oolo/ex4.js
@@ -1,10 +1,11 @@
-function Widget(width,height) {
+// ex1: Prorotypal inheritance
+function Widget(width, height) {
 	this.width = width || 50;
 	this.height = height || 50;
 	this.$elem = null;
 }
 
-Widget.prototype.render = function($where){
+Widget.prototype.render = function ($where) {
 	if (this.$elem) {
 		this.$elem.css({
 			width: this.width + "px",
@@ -13,29 +14,78 @@ Widget.prototype.render = function($where){
 	}
 };
 
-function Button(/* ... */) {
-/*
-	...
+function Button(width, height, label) {
+	Widget.call(this, width, height);
+	this.label = label;
 	this.$elem = $("<button>").text(this.label);
-*/
 }
 
-/*
-Button -> render = function($where) {
-	// call the parent render()
-	// add a click handler -> onClick
+Button.prototype = Object.create(Widget.prototype);
+
+Button.prototype.onClick = function (action) {
+	this.$elem.bind('click', action.bind(this))
 }
 
-Button -> onClick = function(evt) {
-	console.log("...");
-}
-
-$(document).ready(function(){
+$(document).ready(function () {
 	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
-
+	var btn1 = new Button(150, 40, 'Button1');
+	var btn2 = new Button(150, 40, 'Button2');
+	btn1.onClick(function (evt) {
+		alert(`${this.label}`);
+	});
 	btn1.render($body);
+
+	btn2.onClick(function (evt) {
+		alert(`${this.label}`);
+	});
 	btn2.render($body);
 });
-*/
+
+
+// ex2 : OLOO Style
+// var Widget = {
+// 	init: function (width, height, $elem) {
+// 		this.width = width;
+// 		this.height = height;
+// 		this.$elem = null;
+// 	},
+// 	render: function ($where) {
+// 		if (this.$elem) {
+// 			this.$elem.css({
+// 				width: this.width + "px",
+// 				height: this.height + "px"
+// 			}).appendTo($where);
+// 		}
+// 	}
+// }
+
+// var Button = Object.create(Widget);
+// // Button.init = function (width, height, label) {
+// // 	this.width = width;
+// // 	this.height = height;
+// // 	this.label = label;
+// // 	this.$elem = $("<button>").text(this.label);
+// // },
+
+// Button.onClick = function (action) {
+// 	this.$elem.bind('clicked', action.bind(this))
+// }
+
+// $(document).ready(function () {
+// 	var $body = $(document.body);
+
+// 	var btn1 = Object.create(Button);
+// 	btn1.init(150, 40, 'Button1');
+// 	btn1.onClick(function (evt) {
+// 		alert(`${this.label}`);
+// 	});
+// 	btn1.render($body);
+
+
+// 	var btn2 = Object.create(Button);
+// 	btn2.init(150, 40, 'Button2');
+// 	btn2.onClick(function (evt) {
+// 		alert(`${this.label}`);
+// 	});
+// 	btn2.render($body);
+// });
